8fc35f7b15bebbcd78b8b4773bf1dd2484fb3da8
Jan Christoph Ebersbach <jceb@e-jc.de>
2021-07-19 18:41:12 +0200

    feat: add more type information

--- variables.tf
+++ variables.tf
@@ -1,25 +1,30 @@
 variable "hcloud_token" {
   description = "Hetzner cloud auth token"
+  type        = string
 }
 
 variable "cluster_name" {
   description = "Cluster name (prefix for all resource names)"
   default     = "hetzner"
+  type        = string
 }
 
 variable "datacenter" {
   description = "Hetzner datacenter where resources resides, hel1-dc2 (Helsinki 1 DC 2) or fsn1-dc14 (Falkenstein 1 DC14)"
   default     = "hel1-dc2"
+  type        = string
 }
 
 variable "image" {
   description = "Node boot image"
   default     = "ubuntu-20.04"
+  type        = string
 }
 
 variable "master_type" {
   description = "Master node type (size)"
   default     = "cx21" # 2 vCPU, 4 GB RAM, 40 GB Disk space
+  type        = string
 }
 
 variable "ssh_keys" {
@@ -29,6 +34,7 @@ variable "ssh_keys" {
 
 variable "k3s_channel" {
   default = "stable"
+  type    = string
 }
 
 variable "node_groups" {
@@ -42,7 +48,7 @@ variable "node_groups" {
 variable "floating_ips" {
   description = "Map of floating IPs, key is ip_type (ipv4, ipv6), value is count of IPs in group"
   type        = map(string)
-  default = { }
+  default     = {}
 }
 
 variable "master_user_data" {
