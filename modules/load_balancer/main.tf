de2a9470713b19a309613576690f775df57f4c75
Jan Christoph Ebersbach <jceb@e-jc.de>
2021-07-20 10:08:10 +0200

    fix: variable name

--- modules/load_balancer/main.tf
+++ modules/load_balancer/main.tf
@@ -18,7 +18,7 @@ resource "hcloud_load_balancer_service" "load_balancer_service" {
 }
 
 resource "hcloud_load_balancer_target" "load_balancer_target" {
-  for_each = var.target_servers
+  for_each = var.target
 
   type             = "server"
   load_balancer_id = hcloud_load_balancer.load_balancer.id
