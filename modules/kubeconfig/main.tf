7d809255511f66f6121f465034d5f97fa74c1366
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2022-02-08 13:09:36 +0100

    style: reindent configuration code

--- modules/kubeconfig/main.tf
+++ modules/kubeconfig/main.tf
@@ -1,27 +1,27 @@
 resource "local_file" "setkubeconfig" {
-    filename = "./setkubeconfig"
-    content  = templatefile(
-      "${path.module}/templates/setkubeconfig", {
-        cluster_name = var.cluster_name
-        master_ipv4 = var.master_ipv4
-      }
-    )
+  filename = "./setkubeconfig"
+  content = templatefile(
+    "${path.module}/templates/setkubeconfig", {
+      cluster_name = var.cluster_name
+      master_ipv4  = var.master_ipv4
+    }
+  )
 
-    file_permission = "0755"
+  file_permission = "0755"
 }
 
 resource "local_file" "unsetkubeconfig" {
-    filename = "./unsetkubeconfig"
-    content  = templatefile(
-      "${path.module}/templates/unsetkubeconfig", {
-        cluster_name = var.cluster_name
-      }
-    )
-
-    file_permission = "0755"
-
-    provisioner "local-exec" {
-        when    = destroy
-        command = "./unsetkubeconfig"
+  filename = "./unsetkubeconfig"
+  content = templatefile(
+    "${path.module}/templates/unsetkubeconfig", {
+      cluster_name = var.cluster_name
     }
+  )
+
+  file_permission = "0755"
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "./unsetkubeconfig"
+  }
 }
