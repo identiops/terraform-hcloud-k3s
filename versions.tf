906734f1a9674c6958e15668e07e990c69ef087a
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2024-01-19 19:26:49 +0100

    chore: bump hcloud version

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = "1.32.2"
+      version = "1.45.0"
     }
   }
   required_version = ">= 0.13"
