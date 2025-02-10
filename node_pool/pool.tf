ab39cb943f30e5ec0806dc287fd75f5cf294969a
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2025-02-10 13:27:35 +0100

    chore: update terraform versions

--- node_pool/pool.tf
+++ node_pool/pool.tf
@@ -1,18 +1,6 @@
 # Copyright 2024, identinet GmbH. All rights reserved.
 # SPDX-License-Identifier: MIT
 
-terraform {
-  required_providers {
-    # See Version Constraints: https://developer.hashicorp.com/terraform/language/expressions/version-constraints
-    hcloud = {
-      # Documentation; https://registry.terraform.io/providers/hetznercloud/hcloud
-      source  = "hetznercloud/hcloud"
-      version = "~> 1.49.0"
-    }
-  }
-  required_version = "~> 1.0"
-}
-
 resource "hcloud_server" "pool" {
   depends_on = [hcloud_placement_group.pool]
 
--- node_pool/versions.tf
+++ node_pool/versions.tf
@@ -9,26 +9,7 @@ terraform {
       source  = "hetznercloud/hcloud"
       version = "~> 1.49.1"
     }
-    http = {
-      source  = "hashicorp/http"
-      version = "~> 3.4.5"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.5.2"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6.3"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = "~> 0.12.1"
-    }
   }
   required_version = "~> 1.0"
 }
 
-provider "hcloud" {
-  token = var.hcloud_token
-}
