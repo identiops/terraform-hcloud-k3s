f6c768a0e3b430a0a290e086e39cc3be8daa3cfa
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2025-06-02 14:09:54 +0200

    ci(flake): update dependencies

--- .flake/flake.nix
+++ .flake/flake.nix
@@ -6,7 +6,7 @@
 
   # inputs.nixpkgs.url = "github:identinet/nixpkgs/identinet";
   # inputs.nixpkgs_unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs =
